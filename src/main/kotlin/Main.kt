cb09a25bd95782880e8748f7c3c66cb59e6e6097
jsk267 <67058273+jsk267@users.noreply.github.com>
2022-09-08 13:42:15 +0900

    실습 2. 카테고리 선택하였을 때 예외처리에 대한 메시지 출력

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,10 +3,27 @@ fun main(args: Array<String>) {
     println("쇼핑을 계속 하시려면 이름을 입력해주세요 :")
 
     val name = readLine()
-    print("""
+    println("""
         감사합니다. 반갑습니다 $name 님
         원하시는 카테고리를 입력해주세요.
         =================================
     """.trimIndent())
 
+    val categories = arrayOf("패션", "전자기기", "반려동물용품")
+    for (categorie in categories){
+        println(categorie)
+    }
+    println("장바구니로 이동할려면 #을 입력해주세요")
+
+    var selectedCategorie = readLine()
+    while(selectedCategorie.isNullOrBlank()) {
+        println("값을 입력해주세요")
+        selectedCategorie = readLine()
+    }
+    if(selectedCategorie == "#"){
+        // 장바구니로 이동해야됨!!
+    }else{
+        // 카테고리에 있는 품목을 골랐을 경우
+        // 카테고리에 없는 품목을 골랐을 경우
+    }
 }
